67180c90ee51cf15eba13b2c5e4fac56976e16c2
geniuslat <traber73@yandex.ru>
2025-01-14 01:09:05 +0500

    immunity check

--- src/bot_logic.py
+++ src/bot_logic.py
@@ -2,6 +2,7 @@ import telebot
 import conclusion
 import setting
 from datetime import datetime
+import time
 
 my_setting = setting.settings()
 logger_chat = my_setting.logger_chat
@@ -11,19 +12,39 @@ toxicity_threshold = my_setting.toxicity_threshold
 
 bot=telebot.TeleBot(token)
 
+def check_status(message):
+    user_status = bot.get_chat_member(message.chat.id, message.from_user.id).status
+    return user_status == 'administrator' or user_status == 'creator'
+
+
+def mute_user_for(message, duration=24*60):
+    user_status = bot.get_chat_member(message.chat.id, message.from_user.id).status
+    if user_status == 'administrator' or user_status == 'creator':
+        bot.reply_to(message, "Невозможно замутить администратора.")
+        return
+    bot.restrict_chat_member(message.chat.id, message.from_user.id, until_date=time.time()+duration*60)
+
+
 def check_for_ban(message):
     my_conclusion = conclusion.conclusion(message, my_setting)
 
     if not my_conclusion.warning:
        print("Не о чем волноваться")
        return
-
+    
+    status = check_status(message)
     user_info = f"Пользователь: {message.from_user.first_name} {message.from_user.last_name} (ID: {message.from_user.id})\n"
     user_info += f"Чат: {message.chat.title if message.chat.title else 'Личный чат'} (ID: {message.chat.id})\n"
     user_info += f"Текст сообщения: {message.text}\n"
+    user_info += f"Имунитет: { 'Да'  if status else 'Нет'}\n"
     user_info += f"Время: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
     user_info += str(my_conclusion)
     bot.send_message(logger_chat,user_info)
+
+    if not status:
+        mute_user_for(message)
+    else:
+        bot.reply_to(message, "Приговор не может быть приведён в исполнение, так как пользователь имунен к стандартной санкции")
 
 
 def validate(message):
