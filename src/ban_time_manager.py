898303258b6612b203d3a233d1fd43cf5bf4b6cf
geniuslat <traber73@yandex.ru>
2025-01-16 22:21:06 +0500

    ban_time_manager

---
+++ src/ban_time_manager.py
@@ -0,0 +1,27 @@
+import json
+
+class UserTracker:
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = self.load_data()
+
+    def load_data(self):
+        try:
+            with open(self.filename, 'r') as file:
+                data = json.load(file)
+                print(data)
+                return data
+        except FileNotFoundError:
+            return {}
+
+    def save_data(self):
+        with open(self.filename, 'w') as file:
+            json.dump(self.data, file)
+
+    def update_user(self, user_id, days):
+        self.data[str(user_id)] = days
+        self.save_data()
+
+    def get_user_days(self, user_id):
+        return self.data.get(str(user_id), 0)
+    
--- src/bot_logic.py
+++ src/bot_logic.py
@@ -1,7 +1,8 @@
 import telebot
 import conclusion
 import setting
-from datetime import datetime
+from ban_time_manager import UserTracker
+from datetime import datetime, timedelta
 import time
 
 my_setting = setting.settings()
@@ -9,6 +10,10 @@ logger_chat = my_setting.logger_chat
 special_chat = my_setting.special_chat
 token = my_setting.token
 toxicity_threshold = my_setting.toxicity_threshold
+tracker = UserTracker(f"{special_chat}_ban_time.json")
+
+days = tracker.get_user_days(463234260)
+print(days)
 
 bot=telebot.TeleBot(token)
 
@@ -16,13 +21,22 @@ def check_status(message):
     user_status = bot.get_chat_member(message.chat.id, message.from_user.id).status
     return user_status == 'administrator' or user_status == 'creator'
 
+def next_midnight():
+    now = datetime.now()
+    midnight_today = datetime(now.year, now.month, now.day)
+    if now >= midnight_today:
+        midnight_today += timedelta(days=1)
+    
+    return midnight_today
 
-def mute_user_for(message, duration=24*60):
+def mute_user_for(message, duration_in_days=1):
     user_status = bot.get_chat_member(message.chat.id, message.from_user.id).status
     if user_status == 'administrator' or user_status == 'creator':
         bot.reply_to(message, "Невозможно замутить администратора.")
         return
-    bot.restrict_chat_member(message.chat.id, message.from_user.id, until_date=time.time()+duration*60)
+
+    
+    bot.restrict_chat_member(message.chat.id, message.from_user.id, until_date=next_midnight()+timedelta(days=duration_in_days))
 
 
 def check_for_ban(message):
@@ -41,15 +55,32 @@ def check_for_ban(message):
     user_info += str(my_conclusion)
     bot.send_message(logger_chat,user_info)
 
+    reply_message = get_conclusion_message(my_conclusion)
     if not status:
-        mute_user_for(message)
+        ban_time = choose_ban_time(message)
+        mute_user_for(message, ban_time)
+        tracker.update_user(message.from_user.id, ban_time)
+        reply_message += f"\nМьют выдан на {ban_time} дней"
+        
     else:
-        bot.reply_to(message, "Приговор не может быть приведён в исполнение, так как пользователь имунен к стандартной санкции")
+        return
+        reply_message += f"\n Приговор не может быть приведён в исполнение, так как пользователь имунен к стандартной санкции"
+        
+    bot.reply_to(message, reply_message)
 
 
 def validate(message):
    my_conclusion = conclusion.conclusion(message, my_setting)
    return str(my_conclusion)
+
+def choose_ban_time(message) ->int:
+    last_ban_time = tracker.get_user_days(message.from_user.id)
+    if last_ban_time == 0:
+        return 1
+    return last_ban_time*2
+
+def get_conclusion_message(my_conclusion: conclusion.conclusion ):
+    return str(my_conclusion)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
